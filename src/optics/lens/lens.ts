695a5a6469cb26414f777b2d863d10f3b7abc261
Fabian Cook <hello@fabiancook.dev>
2020-09-05 01:35:24 +1200

    Lens source, allow async iterable as source

--- src/optics/lens/lens.ts
+++ src/optics/lens/lens.ts
@@ -1,22 +1,70 @@
-export interface Lens<T> extends Iterable<T>, AsyncIterable<[Iterable<T>, Iterable<T>]> {
+import {unlink} from "fs";
+
+export interface LensRay<T> {
+    sequence: number
+    value: T
+}
+
+export interface LensChange<T> {
+    additions: Iterable<LensRay<T>>
+    deletions: Iterable<LensRay<T>>
+}
+
+function isOpenable(value: unknown): value is { open: boolean } {
+    function isOpenableLike(value: unknown): value is { open: unknown } {
+        return !!value
+    }
+    return (
+        isOpenableLike(value) &&
+        typeof value.open === "boolean"
+    )
+}
+
+function isAsyncIterable<TBind>(value: unknown): value is AsyncIterable<TBind> {
+    function isAsyncIterableLike(value: unknown): value is { [Symbol.asyncIterator]: unknown } {
+        return !!value
+    }
+    return (
+        isAsyncIterableLike(value) &&
+        typeof value[Symbol.asyncIterator] === "function"
+    )
+}
+
+// If Iterable & AsyncIterable, must be C
+export type LensSource<T, C extends LensChange<T> = LensChange<T>> = Iterable<T> | (Iterable<T> & AsyncIterable<C>)
+
+export interface Lens<T, C extends LensChange<T> = LensChange<T>, S extends LensSource<T, C> = LensSource<T, C>> extends Iterable<T>, AsyncIterable<C> {
     open: boolean
 }
 
-export class Lens<T> implements Lens<T> {
+export class Lens<T, C extends LensChange<T> = LensChange<T>, S extends LensSource<T, C> = LensSource<T, C>> implements Lens<T, C> {
 
-    readonly #staticSource: Iterable<T>
+    protected source?: S
 
-    constructor(staticSource: Iterable<T> = []) {
-        this.#staticSource = staticSource
-        this.open = false
+    constructor(source?: S) {
+        this.source = source
+        if (isOpenable(source)) {
+            Object.defineProperty(this, "open", {
+                get(): boolean {
+                    return source.open
+                }
+            })
+        } else {
+            // If we have an async iterable without an open boolean, then we will mark it as always open, hit the async iterator to know more
+            this.open = isAsyncIterable(source)
+        }
     }
 
     *[Symbol.iterator]() {
-        yield* this.#staticSource
+        if (this.source) {
+            yield* this.source
+        }
     }
 
-    async *[Symbol.asyncIterator](): AsyncIterableIterator<[Iterable<T>, Iterable<T>]> {
-        // No additions or deletions, always static
+    async *[Symbol.asyncIterator]() {
+        if (isAsyncIterable<C>(this.source)) {
+            yield* this.source
+        }
     }
 }
 
@@ -30,31 +78,31 @@ export function array<T>(lens: Lens<T>): T[] {
     return [...lens]
 }
 
-export async function* additions<T>(lens: Lens<T>): AsyncIterable<Iterable<T>> {
-    for await (const [additions] of lens) {
+export async function* additions<T, C extends LensChange<T>>(lens: Lens<T, C>): AsyncIterable<C["additions"]> {
+    for await (const { additions } of lens) {
         yield additions
     }
 }
 
-export async function* deletions<T>(lens: Lens<T>): AsyncIterable<Iterable<T>> {
-    for await (const [, deletions] of lens) {
+export async function* deletions<T, C extends LensChange<T>>(lens: Lens<T, C>): AsyncIterable<C["deletions"]> {
+    for await (const { deletions } of lens) {
         yield deletions
     }
 }
 
-export async function next<T>(lens: Lens<T>): Promise<[Iterable<T>, Iterable<T>] | undefined> {
+export async function onChange<T, C extends LensChange<T>>(lens: Lens<T, C>): Promise<C | undefined> {
     const iterator = lens[Symbol.asyncIterator]()
     const next = await iterator.next()
     return next.done ? undefined : (next.value ?? undefined)
 }
 
-export async function nextAddition<T>(lens: Lens<T>): Promise<Iterable<T> | undefined> {
+export async function onAddition<T, C extends LensChange<T>>(lens: Lens<T, C>): Promise<C["additions"] | undefined> {
     const iterator = additions(lens)[Symbol.asyncIterator]()
     const next = await iterator.next()
     return next.done ? undefined : (next.value ?? undefined)
 }
 
-export async function nextDeletion<T>(lens: Lens<T>): Promise<Iterable<T> | undefined> {
+export async function onDeletion<T, C extends LensChange<T>>(lens: Lens<T, C>): Promise<C["deletions"] | undefined> {
     const iterator = additions(lens)[Symbol.asyncIterator]()
     const next = await iterator.next()
     return next.done ? undefined : (next.value ?? undefined)
